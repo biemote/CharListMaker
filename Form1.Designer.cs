7aa1be698e013f13e7aadd548309c1ecb187bc7b
biemote <asura@trastrium.xyz>
2022-09-17 17:05:27 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace CharListMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_openTextFile = new System.Windows.Forms.Button();
+            this.button_convert = new System.Windows.Forms.Button();
+            this.richTextBox_logs = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button_openTextFile
+            // 
+            this.button_openTextFile.Location = new System.Drawing.Point(12, 12);
+            this.button_openTextFile.Name = "button_openTextFile";
+            this.button_openTextFile.Size = new System.Drawing.Size(148, 63);
+            this.button_openTextFile.TabIndex = 0;
+            this.button_openTextFile.Text = "打开文件";
+            this.button_openTextFile.UseVisualStyleBackColor = true;
+            this.button_openTextFile.Click += new System.EventHandler(this.button_openTextFile_Click);
+            // 
+            // button_convert
+            // 
+            this.button_convert.Location = new System.Drawing.Point(166, 12);
+            this.button_convert.Name = "button_convert";
+            this.button_convert.Size = new System.Drawing.Size(148, 63);
+            this.button_convert.TabIndex = 1;
+            this.button_convert.Text = "生成Charlist";
+            this.button_convert.UseVisualStyleBackColor = true;
+            this.button_convert.Click += new System.EventHandler(this.button_convert_Click);
+            // 
+            // richTextBox_logs
+            // 
+            this.richTextBox_logs.Location = new System.Drawing.Point(14, 90);
+            this.richTextBox_logs.Name = "richTextBox_logs";
+            this.richTextBox_logs.ReadOnly = true;
+            this.richTextBox_logs.Size = new System.Drawing.Size(933, 222);
+            this.richTextBox_logs.TabIndex = 2;
+            this.richTextBox_logs.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(958, 324);
+            this.Controls.Add(this.richTextBox_logs);
+            this.Controls.Add(this.button_convert);
+            this.Controls.Add(this.button_openTextFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Charlist Maker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_openTextFile;
+        private System.Windows.Forms.Button button_convert;
+        private System.Windows.Forms.RichTextBox richTextBox_logs;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharListMaker
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<char,Int32> dic;
+        public String resultFilePath;
+        public ArrayList srdArrayList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button_convert.Enabled = false;
+        }
+
+        public int addLogs(string s)
+        {
+            string timeTag = DateTime.Now.ToString("[HH:mm:ss] ");
+            if(richTextBox_logs.InvokeRequired)
+            {
+                richTextBox_logs.Invoke(new Action(() =>
+                {
+                    richTextBox_logs.AppendText(timeTag + s + "\n");
+                    richTextBox_logs.ScrollToCaret();
+                }));
+            }
+            else
+            {
+                richTextBox_logs.AppendText(timeTag + s + "\n");
+                richTextBox_logs.ScrollToCaret();
+            }
+            return 0;
+        }
+
+        private void button_openTextFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text File|*.txt|All Files|*.*";
+            ofd.Multiselect = true;
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                srdArrayList = new ArrayList();
+                string[] fPaths = ofd.FileNames;
+                foreach(string s in fPaths)
+                {
+                    srdArrayList.Add(new StreamReader(s, true));
+                }
+                resultFilePath = System.IO.Path.GetDirectoryName(fPaths[0]);
+                button_convert.Enabled = true;
+                button_openTextFile.Enabled = false;
+                addLogs("已添加" + fPaths.Length.ToString()  + "个文档。");
+            }
+        }
+
+        private void button_convert_Click(object sender, EventArgs e)
+        {
+            dic = new Dictionary<char,Int32>();
+            dic.Clear();
+            try
+            {
+                foreach(StreamReader stream in srdArrayList)
+                {
+                    Int32 chara = stream.Read();
+                    while (chara != -1)
+                    {
+                        if (!dic.ContainsKey((char)chara))
+                        {
+                            dic.Add((char)chara, chara);
+                        }
+                        chara = stream.Read();
+                    }
+                    stream.Close();
+                }
+
+                Int32[] illegal = { 10, 13};
+                foreach(Int32 sage in illegal)
+                {
+                    if (dic.ContainsKey((char)sage))
+                    {
+                        dic.Remove((char)sage);
+                    }
+                }
+
+                var val = dic.Keys.ToList();
+                val.Sort();
+
+                string timeTag = DateTime.Now.ToString("[HH_mm_ss]");
+                if (!Directory.Exists(resultFilePath + "\\Result"))
+                {
+                    Directory.CreateDirectory(resultFilePath + "\\Result");
+                }
+                string resultFile = resultFilePath + "\\Result\\" + timeTag +".txt";
+                StreamWriter sw = new StreamWriter(resultFile, true, Encoding.Unicode);
+                foreach (var key in val)
+                {
+                    sw.Write((char)key);
+                }
+
+                sw.Close();
+                srdArrayList.Clear();
+                dic.Clear();
+
+                button_convert.Enabled = false;
+                button_openTextFile.Enabled = true;
+
+                addLogs("Charlist已生成至路径：" + resultFile);
+            }
+            catch
+            {
+                addLogs("yeah~程序炸了~");
+                button_convert.Enabled = false;
+                button_openTextFile.Enabled = true;
+                srdArrayList.Clear();
+                dic.Clear();
+            }
+        }
+    }
+}
